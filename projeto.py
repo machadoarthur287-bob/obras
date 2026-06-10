752def281cbbf1beec230831f4af3df3fdccfb0e
jovemtech <jovem@UNR04DT0065143.animaedu.intranet>
2026-06-10 16:39:58 -0300

    adicionei as funcoes um e dois

---
+++ projeto.py
@@ -0,0 +1,36 @@
+materials = ['cimento','brita','areia','tijolos']
+enginner = ['carmen']
+servant = ['joaõ','carlos','tobias']
+MasterBuilde = ['robson']
+
+def materiais():
+    print('opçoes de maeriais')
+    print('modificação na lista de materias liberados')
+    funcoesMaterias = input("ajusta a lista:\n1 - Lista da obra\n2 - adicionar materias\n3 - tirar materias\n")
+    match funcoesMaterias:
+        case "1":
+            print (materials)
+        case '2':
+            adicona = input('escreva o que quer adicionar')
+            materials.append(adicona)
+            print(materials)
+        case '3':
+            tira = input('escreva oque quer tirar')
+            materials.remove(tira)
+            print(materials)
+materiais()
+
+def fucionarios():
+    print('acesso liberado a lista de serventes')
+    FuncoesMaterias = input("ajusta a lista:\n1 - lista de empregados\n2 - contradas\n3 - tirar materias\n")
+    match funcoesMaterias:
+        case "1":
+            print (materials)
+        case '2':
+            adicona = input('escreva o que quer adicionar')
+            materials.append(adicona)
+            print(materials)
+        case '3':
+            tira = input('escreva oque quer tirar')
+            materials.remove(tira)
+            print(materials)
